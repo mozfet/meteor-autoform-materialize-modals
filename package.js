e5862f367e324df52428bb5bbf2c012cbd3b57be
Wynand <>
2018-02-04 20:55:26 +0200

    Bump Version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'mozfet:autoform-materialize-modals',
   summary: 'Autoform Materialize Modals',
-  version: '1.1.4',
+  version: '1.1.5',
   git: 'https://github.com/mozfet/meteor-autoform-materialize-modals.git'
 });
 
