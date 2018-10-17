c0b8e471dd64567099282c8cef68626b8c8d65a2
Wynand Vermeulen <>
2018-10-17 13:56:29 +0200

    dependancy chain update

--- package.js
+++ package.js
@@ -1,13 +1,14 @@
 Package.describe({
   name: 'mozfet:autoform-materialize-modals',
   summary: 'Autoform Materialize Modals',
-  version: '4.0.2',
+  version: '4.0.3',
   git: 'https://github.com/mozfet/meteor-autoform-materialize-modals.git'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@1.4');
-  api.use(['templating', 'blaze', 'underscore'], 'client');
+  api.versionsFrom('METEOR@1.6');
+  api.use(['templating@1.3.0', 'blaze@2.3.0'], 'client');
+  api.use(['underscore'], 'client');
   api.use('ecmascript');
   api.addFiles([
     'index.js'
